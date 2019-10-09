d6b38b3e34aad182eb6ff72da068159f35d249ec
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:41:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
 def my_collect(array)
   i = 0
+  first_name = []
   while i < array.size do
-    yield(array[i]) if block_given?
+    first_name.push(yield(array[i])) if block_given?
     i += 1
   end
+
+  first_name
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
